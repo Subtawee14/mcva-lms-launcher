c32a98fad1c20ee925a5425d9ce2ac5a7d9d4960
subtawee <subtawee.hanyut@agoda.com>
2023-08-12 14:10:16 +0700

    change local url

--- src/components/CustomForm.tsx
+++ src/components/CustomForm.tsx
@@ -83,8 +83,7 @@ const CustomForm: React.FC = () => {
       >
         <Form.Item label="Environment" name="environment" required>
           <Radio.Group onChange={onEnvironmentChange}>
-            <Radio.Button value="https://b0d2-2001-fb1-c5-5c9-a0cd-5b78-307c-7992.ngrok.io/api/connect">
-            {/* <Radio.Button value="http://localhost:3000/api/connect"> */}
+            <Radio.Button value="http://localhost:3000/api/connect">
               Local
             </Radio.Button>
             {/* <Radio.Button value="https://dev-map.mycourseville.com/api/connect"> */}
